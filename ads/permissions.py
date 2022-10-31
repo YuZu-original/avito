e5553e6498bc9b88e92e05418e927eef55bc5721
YuZu <yurkazubkov@yandex.ru>
2022-10-31 21:17:17 +0300

    upd permissions messages

--- ads/permissions.py
+++ ads/permissions.py
@@ -4,12 +4,16 @@ from users.models import User
 
 
 class SelectionEditPermission(BasePermission):
+    message = "It's not your selection!"
+
     def has_object_permission(self, request, view, obj):
-        return bool(request.user.role in [User.MEMBER, User.ADMIN] or
+        return bool(request.user.role in [User.MODERATOR, User.ADMIN] or
                     request.user.id == obj.owner_id)
 
 
 class AdEditPermission(BasePermission):
+    message = "It's not your ad!"
+
     def has_object_permission(self, request, view, obj):
-        return bool(request.user.role in [User.MEMBER, User.ADMIN] or
+        return bool(request.user.role in [User.MODERATOR, User.ADMIN] or
                     request.user.id == obj.author_id)
